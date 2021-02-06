d1f9ac310dbfcec250fc2b6cc38b48a4205e4435
Anish Sebastian <anishst@hotmail.com>
2021-02-06 16:02:41 -0500

    added hello route

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 
 Basic [Java Spring Boot](https://spring.io/projects/spring-boot) application
 
+# How to Run
 
+1. use IDE or use command: ```mvnw spring-boot:run``` from project root
 ## To Do
 
-- [ x ] [setup IntelliJ IDE](https://www.youtube.com/watch?v=H_XxH66lm3U&t=84s)
-- [ ] create a basic application using [sprint initializer](https://start.spring.io/)
+- [x] [setup IntelliJ IDE](https://www.youtube.com/watch?v=H_XxH66lm3U&t=84s)
+- [x] create a basic application using [sprint initializer](https://start.spring.io/)
     - [Video Example](https://www.youtube.com/watch?v=df7Dso9q700&t=565s)
 - [ ] add basic junit tests
 - [ ] add cucumber tests/reports
--- src/main/java/com/example/JavaSpringBoot/JavaSpringBootApplication.java
+++ src/main/java/com/example/JavaSpringBoot/JavaSpringBootApplication.java
@@ -2,12 +2,22 @@ package com.example.JavaSpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JavaSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaSpringBootApplication.class, args);
+	}
+
+	// http://localhost:8081/hello?name=Anish
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
 	}
 
 }
