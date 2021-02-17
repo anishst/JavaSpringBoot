b1dcfa8ec0836d664b0bf4611afacae1cfcd271b
Anish Sebastian <anishst@hotmail.com>
2021-02-17 08:42:03 -0500

    added a selenium test; added cucumber-spring dependency

--- src/test/java/com/example/JavaSpringBoot/HttpRequestTest.java
+++ src/test/java/com/example/JavaSpringBoot/HttpRequestTest.java
@@ -1,5 +1,7 @@
 package com.example.JavaSpringBoot;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -22,5 +24,16 @@ public class HttpRequestTest {
     public void greetingShouldReturnDefaultMessage() throws Exception {
         assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/hello",
                 String.class)).contains("Hello");
+    }
+
+    @Test
+    public void simpleSeleniumTest() {
+        WebDriver driver = null;
+        driver = new ChromeDriver();
+        driver.get("http://localhost:" + port);
+        driver.navigate().to("http://localhost:" + port + "/hello");
+        System.out.println("Inside selenium test");
+        driver.quit();
+
     }
 }
--- src/test/java/com/example/JavaSpringBoot/JavaSpringBootApplicationTests.java
+++ src/test/java/com/example/JavaSpringBoot/JavaSpringBootApplicationTests.java
@@ -1,14 +1,33 @@
 package com.example.JavaSpringBoot;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class JavaSpringBootApplicationTests {
+
+
+	@LocalServerPort
+	private int port;
+
 
 	@Test
 	void contextLoads() {
 		System.out.println("Test");
+	}
+
+	@Test
+	public void simpleSeleniumTest() {
+		WebDriver driver = null;
+		driver = new ChromeDriver();
+		driver.get("http://localhost:" + port);
+		driver.navigate().to("http://localhost:" + port + "/hello");
+		System.out.println("Inside selenium test");
+		driver.quit();
+
 	}
 
 }
